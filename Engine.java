326d63659013808299d55fc5fd544494f9d0b0c7
ReedShaw <rrshaw@smith.edu>
2024-02-20 12:02:13 -0500

    I can write in java

--- Engine.java
+++ Engine.java
@@ -1,3 +1,40 @@
+
 public class Engine {
+
+    FuelType fuelType;
+    double maxFuel;
+    double currentFuel;
+
+    public Engine (FuelType fuel_type, double max_fuel, double current_fuel){
+        this.fuelType = fuel_type;
+        this.maxFuel = max_fuel;
+        self.currentFuel = current_fuel;
+    }
+
+    //Show fuel type
+    public show_fuel_type(){
+        return this.fuelType;
+    }
+
+    //Show max fuel
+    public show_max_fuel(){
+        return this.maxFuel;
+    }
+
+    //Show current fuel
+    public show_current_fuel(){
+        return this.currentFuel;
+    }
+
+    //refuel method
+    public refuel_engine(){
+        self.Current_Fuel = this.maxFuel;
+    
+    }
+    
+    
+
+        
+
 
 }
