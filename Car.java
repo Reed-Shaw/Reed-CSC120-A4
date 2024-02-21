49107537079b078add15cf8c26db70ee5673a361
ReedShaw <rrshaw@smith.edu>
2024-02-20 20:31:36 -0500

    Engine first try, car partly done.

--- Car.java
+++ Car.java
@@ -1,3 +1,26 @@
+import java.util.ArrayList;
+
 public class Car {
+
+    int maxPeople;
+    ArrayList<Passenger> passengers;
+
+    public Car(int max_people){
+        this.maxPeople = max_people;
+        this.passengers = new ArrayList<Passenger>(this.maxPeople);
+      
+    }
+
+    public int getCapacity(){
+        return this.maxPeople;
+    }
+
+    public int seatsRemaining(){
+        return (maxPeople - passengers.size());
+    }
+
+    public void addPassenger(Passenger p){
+        passengers.add(p);
+    }
 
 }
--- Engine.java
+++ Engine.java
@@ -5,36 +5,61 @@ public class Engine {
     double maxFuel;
     double currentFuel;
 
-    public Engine (FuelType fuel_type, double max_fuel, double current_fuel){
+    public Engine(FuelType fuel_type, double max_fuel, double current_fuel){
         this.fuelType = fuel_type;
         this.maxFuel = max_fuel;
-        self.currentFuel = current_fuel;
+        this.currentFuel = current_fuel;
     }
 
     //Show fuel type
-    public show_fuel_type(){
+    public FuelType show_fuel_type(){
         return this.fuelType;
     }
 
     //Show max fuel
-    public show_max_fuel(){
+    public double show_max_fuel(){
         return this.maxFuel;
     }
 
     //Show current fuel
-    public show_current_fuel(){
+    public double show_current_fuel(){
         return this.currentFuel;
     }
 
     //refuel method
-    public refuel_engine(){
-        self.Current_Fuel = this.maxFuel;
-    
+    private void refuel_engine(){
+        this.currentFuel = this.maxFuel;
+    }    
+
+    //go method will reduce fuel amount
+    //True means there is still fuel, False means fuel level is at zero. 
+    public boolean go(){
+        this.currentFuel -= 25;
+        System.out.println(currentFuel);
+        if (this.currentFuel > 0){
+            return true;
+        }
+        else{
+            return false;
+        }
+
     }
-    
-    
+
+    public static void main(String[] args) {
+        Engine myEngine = new Engine(FuelType.ELECTRIC, 100.0, 80);
+        System.out.println(myEngine.show_current_fuel());
+        myEngine.refuel_engine();
+        System.out.println(myEngine.show_current_fuel());
+        System.out.println(myEngine.go());
+        System.out.println(myEngine.go());
+        System.out.println(myEngine.go());
+        myEngine.refuel_engine();
+        while (myEngine.go()) {
+            System.out.println("Choo choo!");
+        }
+        System.out.println("Out of fuel.");
+
 
         
-
-
+    }
 }
